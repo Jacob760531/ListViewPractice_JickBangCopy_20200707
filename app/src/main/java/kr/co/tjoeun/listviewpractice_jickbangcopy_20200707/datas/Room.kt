c3c777b1af97a893682c8f8b89ee48308ef12903
조장우 <jopuk2@gmail.com>
2020-07-07 21:16:00 +0900

    방 하나 표현하는 xml

--- app/src/main/java/kr/co/tjoeun/listviewpractice_jickbangcopy_20200707/datas/Room.kt
+++ app/src/main/java/kr/co/tjoeun/listviewpractice_jickbangcopy_20200707/datas/Room.kt
@@ -1,4 +1,4 @@
-package kr.co.tjoeun.listviewpractice_jickbangcopy_20200707.Datas
+package kr.co.tjoeun.listviewpractice_jickbangcopy_20200707.datas
 
 class Room(val price:Int, val address : String, val floor:Int, val description) {
 
