4523f1ccab01488633e7b1605505fa14df0a71ba
조장우 <jopuk2@gmail.com>
2020-07-07 21:03:55 +0900

    리스트뷰 배치 + 방 정보 데이터클래스 생성

---
+++ app/src/main/java/kr/co/tjoeun/listviewpractice_jickbangcopy_20200707/Datas/Room.kt
@@ -0,0 +1,6 @@
+package kr.co.tjoeun.listviewpractice_jickbangcopy_20200707.Datas
+
+class Room(val price:Int, val address : String, val floor:Int, val description) {
+
+
+}
---
+++ app/src/main/java/kr/co/tjoeun/listviewpractice_jickbangcopy_20200707/room_list.kt
@@ -0,0 +1,12 @@
+package kr.co.tjoeun.listviewpractice_jickbangcopy_20200707
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class room_list : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_room_list)
+    }
+}
