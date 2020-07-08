066d3d46fdcd2b34965cf4cfbcef0a30ee316dd6
조장우 <jopuk2@gmail.com>
2020-07-08 21:37:02 +0900

    스플래쉬 화면 추가 -> 시작점 변경

---
+++ app/src/main/java/kr/co/tjoeun/listviewpractice_jickbangcopy_20200707/SplashActivity.kt
@@ -0,0 +1,21 @@
+package kr.co.tjoeun.listviewpractice_jickbangcopy_20200707
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        setValues()
+        setupEvents()
+    }
+
+    override fun setupEvents() {
+    }
+
+    override fun setValues() {
+    }
+
+}
